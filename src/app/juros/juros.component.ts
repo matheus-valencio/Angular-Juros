bf413ff6971eef26c14ff0ca2d1d72bb3c4604fd
matheus-valencio <matheus.valencio@live.com>
2022-03-18 19:49:44 -0300

    2 files modified, 6 added

---
+++ src/app/juros/juros.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros',
+  templateUrl: './juros.component.html',
+  styleUrls: ['./juros.component.css'],
+})
+export class JurosComponent implements OnInit {
+  vp: number;
+  j: number;
+  n: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
